d776053045bd8f99a437bba1a666e171bc1ba09f
YeOldeEricC <eachapman26@gmail.com>
2022-02-28 16:29:20 +0000

    ...

---
+++ testing/test.c
@@ -0,0 +1,34 @@
+/*
+---------------------------------
+
+HEADER FILE STRUCTURE --
+
+#ifndef TEST_H_
+#define TEST_H_
+
+// debugging fns
+
+// fns
+void fn();
+
+#endif // TEST_H_
+
+---------------------------------
+
+C FILE STRUCTURE --
+
+#include "../h/test.h"
+#include <stdio.h>
+
+// debugging fns
+
+// fns
+void fn() {...}
+
+---------------------------------
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <complex.h>
